b0ac073be4b36564db5847307729de73e0c3d68f
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-09 23:00:01 -0300

    Update TargetValue.cs

--- APIMetrics/Models/TargetValue.cs
+++ APIMetrics/Models/TargetValue.cs
@@ -1,9 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace APIMetrics.Models;
 
 public class TargetValue
 {
-    [Range(minimum: 1, maximum: 100, ErrorMessage = "Informe um valor entre 1 e 100")]
+    [Range(minimum: -100, maximum: 100, ErrorMessage = "Informe um valor entre -100 e 100")]
     public int NewTargetValue { get; set; }
 }
